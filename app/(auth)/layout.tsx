fd89f399b2509735ec271473de43f79a8d2815b7
Diqi Al-Haqqi <dq.alhaqqi@gmail.com>
2024-08-24 13:55:25 +0700

    --wip--

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,17 @@
 import { Link } from 'ui'
 
 import { Logo } from '@/components/logo'
+import { ThemeSwitcher } from '@/components/theme-switcher'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex items-center bg-muted/20 justify-center min-h-svh">
+            <div className="absolute right-4 top-4">
+                <ThemeSwitcher />
+            </div>
             <div className="max-w-lg mx-auto w-full">
-                <Link href="/" className="mx-auto size-10 bg-secondary rounded grid place-content-center mb-6">
-                    <Logo className="size-5" />
+                <Link href="/" className="mx-auto size-10 rounded grid place-content-center mb-6">
+                    <Logo className="size-10" />
                 </Link>
                 {children}
             </div>
